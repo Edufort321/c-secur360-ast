9bf588eb8c3dc6f6ba4224f2c822cd34bfab9c48
Edufort321 <eric.dufort@cerdia.ai>
2025-07-03 19:17:23 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['localhost', 'mdl.ca', 'vercel.app'],
+  },
+  async rewrites() {
+    return [
+      // Redirection des sous-domaines vers les tenants
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: '(?<tenant>.*)\\.mdl\\.ca',
+          },
+        ],
+        destination: '/:tenant/:path*',
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
