1a95ef568064f983dbc6bddb8dd6b6c1a1496aed
Edufort321 <eric.dufort@cerdia.ai>
2025-07-06 20:07:39 -0400

    Delete app/[tenant]/ast/nouveau/ASTFormUltraPremium.tsx

--- app/[tenant]/ast/nouveau/ASTFormUltraPremium.tsx
+++
@@ -1,9 +0,0 @@
-import ASTForm from '@/components/ASTForm';
-
-export default function NouvellASTPage({ 
-  params 
-}: { 
-  params: { tenant: string } 
-}) {
-  return <ASTForm tenant={params.tenant} />;
-}
