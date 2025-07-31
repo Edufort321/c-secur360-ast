ddcc9f0e4acd8548a8e9c75d66dca96a24b01439
Edufort321 <eric.dufort@cerdia.ai>
2025-07-31 17:08:51 -0400

    Update index.tsx

--- components/steps/Step4Permits/index.tsx
+++ components/steps/Step4Permits/index.tsx
@@ -201,7 +201,7 @@ const getPermitModules = (language: 'fr' | 'en'): PermitModule[] => {
         'Photos géolocalisées',
         'Plan de sauvetage intégré'
       ],
-      component: ConfinedSpace
+      component: undefined
     },
     {
       id: 'electrical-work',
