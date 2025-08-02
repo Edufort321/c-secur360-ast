66ea5838eafa50f0d4016bcaf37f7332d47839e5
Edufort321 <eric.dufort@cerdia.ai>
2025-08-02 19:06:08 -0400

    Update AtmosphericTesting.tsx

--- components/steps/Step4Permits/ConfinedSpace/AtmosphericTesting.tsx
+++ components/steps/Step4Permits/ConfinedSpace/AtmosphericTesting.tsx
@@ -1,1 +1,17 @@
+"use client";
+import React from 'react';
 
+interface AtmosphericTestingProps {
+  [key: string]: any;
+}
+
+const AtmosphericTesting: React.FC<AtmosphericTestingProps> = (props) => {
+  return (
+    <div style={{ padding: '20px', color: 'white' }}>
+      <h3>🌬️ Atmospheric Testing - En cours de reconstruction...</h3>
+      <p>Composant en cours de refactoring avec SafetyManager</p>
+    </div>
+  );
+};
+
+export default AtmosphericTesting;
