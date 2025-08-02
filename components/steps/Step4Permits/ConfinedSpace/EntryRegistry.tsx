41a522b545dfbb02837ccd9bc44cda806f94d107
Edufort321 <eric.dufort@cerdia.ai>
2025-08-02 19:06:25 -0400

    Update EntryRegistry.tsx

--- components/steps/Step4Permits/ConfinedSpace/EntryRegistry.tsx
+++ components/steps/Step4Permits/ConfinedSpace/EntryRegistry.tsx
@@ -1,1 +1,17 @@
+"use client";
+import React from 'react';
 
+interface EntryRegistryProps {
+  [key: string]: any;
+}
+
+const EntryRegistry: React.FC<EntryRegistryProps> = (props) => {
+  return (
+    <div style={{ padding: '20px', color: 'white' }}>
+      <h3>👥 Entry Registry - En cours de reconstruction...</h3>
+      <p>Composant en cours de refactoring avec SafetyManager</p>
+    </div>
+  );
+};
+
+export default EntryRegistry;
