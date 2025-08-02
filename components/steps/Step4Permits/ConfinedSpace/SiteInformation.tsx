b7d722a348df6f093050294528345e46efea0bf8
Edufort321 <eric.dufort@cerdia.ai>
2025-08-02 19:05:54 -0400

    Update SiteInformation.tsx

--- components/steps/Step4Permits/ConfinedSpace/SiteInformation.tsx
+++ components/steps/Step4Permits/ConfinedSpace/SiteInformation.tsx
@@ -1,1 +1,17 @@
+"use client";
+import React from 'react';
 
+interface SiteInformationProps {
+  [key: string]: any;
+}
+
+const SiteInformation: React.FC<SiteInformationProps> = (props) => {
+  return (
+    <div style={{ padding: '20px', color: 'white' }}>
+      <h3>🏗️ Site Information - En cours de reconstruction...</h3>
+      <p>Composant en cours de refactoring avec SafetyManager</p>
+    </div>
+  );
+};
+
+export default SiteInformation;
