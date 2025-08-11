15776dc3eecbd97faf2f110eea0066b0562db40a
Edufort321 <eric.dufort@cerdia.ai>
2025-08-11 19:24:34 -0400

    Update AddLocationModal.tsx

--- components/modals/AddLocationModal.tsx
+++ components/modals/AddLocationModal.tsx
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import { X } from 'lucide-react';
-import type { Location } from '@/types/ast';
+import type { Location } from '../../app/types/ast';
 
 interface Props {
   isOpen: boolean;
